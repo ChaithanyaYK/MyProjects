1fef5c773893cb7a5e6c82cf4a5e0f749fc223ad
ChaithanyaYK <chaithu.82009@gmail.com>
2021-06-05 13:18:26 +0530

    This is myproject commit2

---
+++ src/ProgramsJava/multiplesofnumbers.java
@@ -0,0 +1,34 @@
+package ProgramsJava;
+
+public class multiplesofnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int result=multiply(6,10);
+		System.out.println(result);
+
+	}
+	
+	public static int multiply(int i,int j) {
+		
+		int k=1;
+		int sum=0;
+		while(k<=j) {
+			sum=sum+i;
+			k++;
+		}
+		
+		return sum;
+	}
+
+}
+/*public static int multiply(int i, int j) {
+	int k=1;// counter variable
+	int sum=0;
+	
+	if(k<=j) { //k will be increasing where j=10
+		sum=sum+i;    // i=5 and sum will change
+		k++; // k is counter variable helps to iterate elements
+	}
+	return sum;*/
